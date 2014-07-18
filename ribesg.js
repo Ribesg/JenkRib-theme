8a96e6121baeab8cb60d2c503664297a25e6c56f
Ribesg <ribesg@yahoo.fr>
2014-07-18 16:46:45 +0200

    Tweaks

--- ribesg.js
+++ ribesg.js
@@ -1,39 +1,4 @@
 /**
- * This is for testing purpose, at the end images should be replaced in Jenkins' WAR file.
+ * Nope.
+ * @author Ribesg
  */
-
-var _ = jQuery.noConflict(true);
-
-var path = 'http://ci.ribesg.fr/job/JenkRib-theme/lastSuccessfulBuild/artifact/images/';
-
-var replacements = {
-   "plugin/jobConfigHistory/img/confighistory.png": "settings.png",
-   "images/24x24/edit-delete.png": "cancel.png"
-};
-
-var replaceImages = function() {
-   var regex = /\/static\/.+?\/(.+)/;
-
-   _('img').each(function(i, img) {
-
-      var src = _(img).attr('src');
-      console.log("Found img: " + src);
-
-      var regexExec = regex.exec(src);
-      var foundPath;
-      if (regexExec != null) {
-         foundPath = regexExec[1];
-         console.log("\tExtracted subPath from src: " + foundPath);
-      } else {
-         foundPath = src;
-      }
-
-      var replacement = replacements[foundPath];
-      if (replacement != null) {
-         console.log("\tFound replacement: " + replacement);
-         _(img).attr('src', path + replacement);
-      } else {
-         console.log("\tNo replacement found");
-      }
-   });
-};
