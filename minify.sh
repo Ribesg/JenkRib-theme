14f3c7e32a64f782a5c72c55654b2d15e692be40
Ribesg <Ribesg@yahoo.fr>
2014-07-19 16:49:10 +0200

    FML

--- minify.sh
+++ minify.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -xe
 # Requires YUI Compressor as 'yc'
 
-env
-
-for PATH in *.css *.js
+for FILEPATH in *.css *.js
 do
-  FILE=$(basename "$PATH")
+  FILE=$(basename "$FILEPATH")
   TYPE="${FILE##*.}"
   NAME="${FILE%.*}"
   yc --type $TYPE -o "$NAME.min.$TYPE" "$FILE"
