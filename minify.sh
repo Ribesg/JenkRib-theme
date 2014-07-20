afb1e9fc3d76e70df98fe3c634dc37f6639c08d2
Ribesg <Ribesg@yahoo.fr>
2014-07-20 12:33:15 +0200

    Tweaks

--- minify.sh
+++ minify.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -xe
 # Requires Stylus and YUI Compressor as 'yc'
 
-for FILEPATH in *.styl
+for FILEPATH1 in *.styl
 do
-  stylus $(basename "$FILEPATH")
+  FILE1=$(basename "$FILEPATH1")
+  TYPE1="${FILE1##*.}"
+  NAME1="${FILE1%.*}"
+  stylus "$FILE1" -c -p > "$NAME1.min.css"
 done
 
-for FILEPATH in *.css *.js
+for FILEPATH2 in *.js
 do
-  FILE=$(basename "$FILEPATH")
-  TYPE="${FILE##*.}"
-  NAME="${FILE%.*}"
-  yc --type $TYPE -o "$NAME.min.$TYPE" "$FILE"
+  FILE2=$(basename "$FILEPATH2")
+  TYPE2="${FILE2##*.}"
+  NAME2="${FILE2%.*}"
+  yc --type $TYPE2 -o "$NAME2.min.$TYPE2" "$FILE2"
 done
